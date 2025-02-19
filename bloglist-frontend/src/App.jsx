ab2b0a63c87299c131d9b5146dfa2d9a15391c6b
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-02-19 16:54:23 +0800

    finish exercise 5.6-5.11

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs(blogs)
+      setBlogs(blogs.sort((blog1, blog2) => blog2.likes - blog1.likes))
     )
   }, [])
 
@@ -95,6 +95,37 @@ const App = () => {
     }
   }
 
+  const handleUpdate = async (blogObj) => {
+    try {
+      const updatedBlog = await blogService.update(blogObj.id, blogObj)
+      setBlogs(blogs.map((blog) => {
+        if (blog.id === updatedBlog.id) {
+          return updatedBlog
+        } else {
+          return blog
+        }
+      }
+      ))
+      setInfoMessage(`Blog updated`)
+      setTimeout(() => {
+        setInfoMessage(null)
+      }, 5000)
+
+    } catch (exception) {
+      if (exception.response) {
+        setErrorMessage(exception.response.data.error)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      } else {
+        setErrorMessage('something went wrong')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+    }
+  }
+
   const handleLogout = () => {
     setUser(null)
     setInfoMessage(`Logged out`)
@@ -128,9 +159,44 @@ const App = () => {
     </form>
   )
 
+  const handleDeleteBlog = async (blog) => {
+    try {
+      // blogService.deleteBlog(id)
+      if (window.confirm(`remove blog ${blog.title} by ${blog.user.name}`)) {
+        await blogService.deleteBlog(blog.id)
+
+        setBlogs(blogs.filter(b => blog.id !== b.id))
+
+        setInfoMessage(`Blog deleted`)
+        setTimeout(() => {
+          setInfoMessage(null)
+        }, 5000)
+      }
+    } catch (exception) {
+      if (exception.response) {
+        setErrorMessage(exception.response.data.error)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      } else {
+        setErrorMessage('something went wrong')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+    }
+
+  }
+
   const blogFrom = () => {
     return blogs.map(blog =>
-      <Blog key={blog.id} blog={blog} />
+      <Blog
+        key={blog.id}
+        blog={blog}
+        updateBlog={handleUpdate}
+        displayRemove={blog.user && blog.user.username === user.username}
+        deleteBlog={handleDeleteBlog}
+      />
     )
   }
 
@@ -148,16 +214,13 @@ const App = () => {
 
   return (
     <div>
-
       <h2>blogs</h2>
       <Notification message={errorMessage} type={"error"} />
       <Notification message={infoMessage} type={"info"} />
       <p>{user.username} logged in <button onClick={handleLogout}>log out</button></p>
-
       <Togglable buttonLabel={"Create a blog"} ref={blogFormRef}>
-        <BlogForm createBlog={handleCreate}/>
+        <BlogForm createBlog={handleCreate} />
       </Togglable>
-
       {blogFrom()}
     </div>
   )
--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog, updateBlog, displayRemove, deleteBlog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  const showWhenInDetailed = { display: showDetails ? '' : 'none' }
+  const hideWhenInDetailed = { display: showDetails ? 'none' : '' }
+
+  const toggleShowDetails = () => { setShowDetails(!showDetails) }
+
+  const likeBlog = () => {
+    updateBlog({
+      ...blog, 
+      likes: blog.likes + 1
+    })
+  }
+
+  const removeBlog = () => {
+    deleteBlog(blog)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenInDetailed}>
+        <div>{blog.title} {blog.author} <button onClick={toggleShowDetails}>view</button></div>
+      </div>
+      <div style={showWhenInDetailed}>
+        <div>{blog.title} {blog.author} <button onClick={toggleShowDetails}>hide</button></div>
+        <div>{blog.url}</div>
+        <div>{blog.likes} <button onClick={likeBlog}>like</button> </div>
+        <div>{blog.user && blog.user.name}</div>
+        {displayRemove && <div><button onClick={removeBlog}>remove</button></div>}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
